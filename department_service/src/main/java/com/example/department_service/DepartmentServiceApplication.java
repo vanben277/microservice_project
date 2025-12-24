3ab9c5e4fd3ae9653f36870a9386eafc554df185
vanben277 <toducbn@gmail.com>
2025-12-24 11:20:32 +0700

    config eureka-server

--- department_service/src/main/java/com/example/department_service/DepartmentServiceApplication.java
+++ department_service/src/main/java/com/example/department_service/DepartmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.department_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
